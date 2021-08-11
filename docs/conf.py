ea62b0c872ac20e3a6493c380a7e7894e3cd47a2
Pranav-Saxena <71541356+Pranav-Saxena@users.noreply.github.com>
2021-08-11 22:24:42 +0530

    v0.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import re
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-
+sys.path.append(os.path.abspath('extensions'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
